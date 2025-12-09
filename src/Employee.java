a9b9600725004418d0965e90276a10516e476cc7
DEEPIKA-B-29 <deepubalu2021@gmail.com>
2025-12-09 23:55:23 +0530

    UC3

--- src/Employee.java
+++ src/Employee.java
@@ -2,7 +2,8 @@ public class Employee {
     int dailyAttendance;
     int workPerDay=8;
     //int halfDay=4;
-    int hourlyPage=20;
+    int partTime=8;
+    int hourlyWage=20;
     int dailySalary;
     public void calculateAttendance(){
         if(((Math.random()*100)%3)==0){
@@ -18,11 +19,12 @@ public class Employee {
         }
     }
     public void dailyWage(){
-        dailySalary=hourlyPage*workPerDay;
         if(dailyAttendance==1){
+            dailySalary=hourlyWage*workPerDay;
             System.out.println(dailySalary);
         }
         else if(dailyAttendance==2){
+            dailySalary=partTime*hourlyWage;
             System.out.println(dailySalary);
         }else{
             System.out.println("No pay");
